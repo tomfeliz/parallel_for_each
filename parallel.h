587198be3ba3fa53520b0d0f311f43054f338f40
Tom Feliz <tom.feliz@tektronix.com>
2017-09-22 10:38:45 -0700

    Fixed thread pool iterator overrun problem.

--- parallel.h
+++ parallel.h
@@ -3,28 +3,43 @@
 #include <thread>
 #include <vector>
 
+// Template parameters will be inferred in most cases.
 template<typename Iterator, typename Function>
 void parallel_for_each(const Iterator& first, const Iterator& last, const Function& l)
 {
+	// Create threads based on number of virtual cores.
 	const auto numThreads = std::thread::hardware_concurrency();
-	const int64_t chunk = (last - first) / numThreads;
 	std::vector<std::thread> threads;
-	for (Iterator it = first; it != last; it += chunk)
-	{
-		Iterator begin = it;
-		auto distance = std::distance(it, last);
-		Iterator end = (std::distance(it, last) > chunk) ? it + chunk : last;
-//		Iterator end = it + chunk;
 
-		threads.push_back(std::thread([begin, end, l]()
+	// Chunk is the size of each batch.
+	const auto chunk = std::max(static_cast<int>((last - first) / numThreads), 1);
+
+	Iterator it = first;
+	auto moreWork = true;
+	do
+	{
+		// Calculate iterator range (be sure not to overrun).
+		Iterator start = it;
+		Iterator end = (last - start >= chunk) ? start + chunk : last;
+
+		// Create threads and store reference in thread pool.
+		threads.push_back(std::thread([start, end, l]()
 		{
-			for (Iterator i = begin; i != end; ++i)
+			for (Iterator i = start; i != end; ++i)
 			{
-				l(*i);
+				l(*i); // i is an iterator, so must de-reference.
 			}
 		}));
-	}
 
+		// Determine if we're done. Only increment 'it' if there's room (avoid overruns).
+		if ((last - start) >= chunk)
+			it += chunk;
+		else
+			moreWork = false;
+
+	} while (moreWork);
+	
+	// Wait for all threads to complete.
 	std::for_each(begin(threads), end(threads), [](std::thread& th)
 	{
 		th.join();
--- program.cpp
+++ program.cpp
@@ -8,6 +8,7 @@
 
 #include "parallel.h"
 
+// Naive Fibonacci function to load CPU (2^(n-1) recursive calls)
 uint64_t fibonacci(uint64_t n)
 {
 	if (n == 0 || n == 1)
@@ -18,27 +19,32 @@ uint64_t fibonacci(uint64_t n)
 
 int main()
 {
-	std::mutex mutex_cons;
-	std::atomic<int> count = 0;
+	std::mutex mutex_cons; // protest default console output
+	std::atomic<int> count = 0; // atomic result counter
 
-	std::vector<int> data(1000);
+	std::vector<int> data(19); // adjust vector length to increse/decrease running time
+
+	// Populate test data vector with random Fibonacci seeds (in a range)
 	std::default_random_engine rand_eng{};
-	std::uniform_int_distribution<int> dist{ 20, 40 };
+	std::uniform_int_distribution<int> dist{ 20, 45 };
 	std::for_each(begin(data), end(data), [&rand_eng, &dist](int& n)
 	{
 		n = dist(rand_eng);
 	});
 
+	// Report number of threads we'll be using
 	const auto threads = std::thread::hardware_concurrency();
 	std::cout << "Using " << threads << " execution threads." << std::endl;
 
 	const auto startGlobal = clock();
 
+	// Make call to parallel_for_each and do the actual work.
 	parallel_for_each(begin(data), end(data), [&mutex_cons, &count](uint64_t n)
 	{
 		const auto start = clock();
 		auto fib = fibonacci(n);
 
+		// Report the thread id, result, and time to execute each thread.
 		auto finish = clock();
 		mutex_cons.lock();
 		std::cout << "Result " << ++count << ", Thread " << std::this_thread::get_id() 
@@ -47,10 +53,12 @@ int main()
 		mutex_cons.unlock();
 	});
 
+	// Report total execution time.
 	auto finishGlobal = clock();
 	std::cout << "Total execution time: " << static_cast<float>(finishGlobal - startGlobal) / CLOCKS_PER_SEC 
 		<< " seconds" << std::endl;
 
+	// Wait for user to respond
 	std::string temp;
 	std::cout << "Press enter to terminate program..." << std::endl;
 	std::getline(std::cin, temp);
